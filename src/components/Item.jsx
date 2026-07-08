3ece18183403879ed48e807d08754957d94d749d
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-08 17:33:29 +0300

    spot check 2 and 3

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react'
+import React, { Component } from "react";
+import { observer } from "mobx-react";
 
 class Item extends Component {
   checkItem = () => {
-    //your code here
-  }
+    const { item } = this.props;
+    item.completed = !item.completed;
+  };
   editItem = () => {
     //your code here
-  }
+  };
   deleteItem = () => {
     //your code here
-  }
+  };
   render() {
+    let item = this.props.item;
     return (
-      <div className="">
-        {/*   your code here
-            each item should be in an input checkbox
-            it should display the item name and location
-            it should have both an edit button and a delete button
-      */}
-      </div>)
+      <div className={item.completed ? "crossed" : ""}>
+        <input type="checkbox" checked={item.completed} onChange={this.checkItem} />
+        {item.name}
+      </div>
+    );
   }
 }
 
-export default observer(Item)
+export default observer(Item);
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,11 @@ import { Item } from "./stores/Item";
 import { ShoppingList } from "./stores/ShoppingList";
 
 let potatoes = new Item("Potatoes");
+let corn = new Item("Corn");
+let sombreros = new Item("Sombreros");
 let groceryList = new ShoppingList();
 groceryList.list.push(potatoes);
+groceryList.list.push(corn);
+groceryList.list.push(sombreros);
 
 ReactDOM.render(<App store={groceryList} />, document.getElementById("root"));
