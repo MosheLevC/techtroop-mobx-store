08c52a06bd796eb32dec4eee1df9960efed72ea6
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-08 16:57:43 +0300

    spot check 1

--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -1,26 +1,27 @@
 /* eslint-disable */
-import { observable, action, makeObservable } from 'mobx'
-import { Item } from './Item'
-
+import { observable, action, makeObservable } from "mobx";
+import { Item } from "./Item";
 
 export class ShoppingList {
-    constructor() {
-        this.list = []
-        this.length = 0
-        // your code here
+  constructor() {
+    this.list = [];
+    this.length = 0;
 
-    }
-    checkItem = () => {
-        // your code here
-    }
-    addItem = () => {
-        // your code here
-    }
-    editItem = () => {
-        // your code here
-    }
-    deleteItem = () => {
-        // your code here
-    }
+    makeObservable(this, {
+      list: observable,
+      length: observable,
+    });
+  }
+  checkItem = () => {
+    // your code here
+  };
+  addItem = () => {
+    // your code here
+  };
+  editItem = () => {
+    // your code here
+  };
+  deleteItem = () => {
+    // your code here
+  };
 }
-
